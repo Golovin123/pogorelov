a9820d7d15c980f343ace2922be075fe6cae6dda
Your Name <you@example.com>
2022-10-29 17:52:23 +0300

    first

---
+++ practice/work/autarization/ViewModel/AuthorizationVIewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autarization.ViewModel
+{
+   public class AuthorizationVIewModel : BaseViewModel
+    {
+        private string _login;
+        private string _password;
+
+
+        public  string Login { get => _login; set => SetProperty(ref _login, value);  }
+
+        public string Password { get => _password; set => SetProperty(ref _password, value); }
+
+        public AuthorizationVIewModel()
+        {
+
+        }
+    }
+}
